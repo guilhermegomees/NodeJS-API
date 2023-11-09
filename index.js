0bbebb964d26850abc2f83bacf10451938cfcb47
Ryan Nicolau Lopes <61668167+ryannlopes@users.noreply.github.com>
2023-11-09 19:39:13 -0300

    Adicionando os metodos para tabela cart

--- index.js
+++ index.js
@@ -20,30 +20,35 @@ db.connect((err) => {
 });
 
 // Methods GET
-app.get("/getAdmins", util.createGetAllRouteHandler("admin"));
+app.get("/getAdmins",   util.createGetAllRouteHandler("admin"));
 app.get("/getUsuarios", util.createGetAllRouteHandler("usuario"));
 app.get("/getProducts", util.createGetAllRouteHandler("product"));
 app.get("/getCompanies", util.createGetAllRouteHandler("company"));
+app.get("/getCarts", util.createGetAllRouteHandler("cart"));
 app.get("/getAdmins/:id", util.createGetRouteHandler("admin", "idAdmin"));
 app.get("/getUsuarios/:id", util.createGetRouteHandler("usuario", "idUser"));
 app.get("/getProducts/:id", util.createGetRouteHandler("product", "id"));
 app.get("/getCompanies/:id", util.createGetRouteHandler("company", "idCompany"));
+app.get("/getCarts/:id", util.createGetAllRouteHandler("cart", "idCart"));
 app.get("/getProductsByQuantity/:quant", util.createGetQuantityRouteHandler("product"));
 
 // Methods POST
 app.post("/addAdmins", util.createPostRouteHandler("admin", "idAdmin"));
 app.post("/addUsuarios", util.createPostRouteHandler("usuario", "idUser"));
 app.post("/addProducts", util.createPostRouteHandler("product", "id"));
+app.post("/addCarts", util.createPostRouteHandler("cart", "idCart"));
 
 // Methods PUT
 app.put("/updateAdmins/:id", util.createPutRouteHandler("admin", "idAdmin"));
 app.put("/updateUsuarios/:id", util.createPutRouteHandler("usuario", "idUser"));
 app.put("/updateProducts/:id", util.createPutRouteHandler("product", "id"));
+app.put("/updateCarts/:id", util.createPutRouteHandler("cart", "idCart"));
 
 // Methods DELETE
 app.delete("/deleteAdmins/:id", util.createDeleteRouteHandler("admin", "idAdmin"));
 app.delete("/deleteUsuarios/:id", util.createDeleteRouteHandler("usuario", "idUser"));
 app.delete("/deleteProducts/:id", util.createDeleteRouteHandler("product", "id"));
+app.delete("/deleteCarts/:id", util.createDeleteRouteHandler("cart", "idCart"));
 
 // Start server
 app.listen(port, () => {
